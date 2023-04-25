6168dea037066d0f081bf1c93b6f1a9336cfe536
Amir <iknubs@gmail.com>
2023-04-24 17:01:43 -0700

    Saving env stuff

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="bg-green-500 w-screen h-screen flex items-center">
+      <div className="w-full text-center">
+        <button className="bg-white p-2 px-4 rounded-lg ">
+          Sign in with google
+        </button>
+      </div>
+    </div>
+  );
 }
