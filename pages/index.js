97043c888cf8ed83c75e8e5e49001f90dca0adc4
Amir <iknubs@gmail.com>
2023-04-24 17:40:07 -0700

    Connecting to mongo

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import Image from "next/image";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-  return (
-    <div className="bg-green-500 w-screen h-screen flex items-center">
-      <div className="w-full text-center">
-        <button className="bg-white p-2 px-4 rounded-lg ">
-          Sign in with google
-        </button>
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-green-500 w-screen h-screen flex items-center">
+        <div className="w-full text-center">
+          <button
+            className="bg-white p-2 px-4 rounded-lg text-black hover:bg-green-100"
+            onClick={() => signIn("google")}
+          >
+            Sign in with google
+          </button>
+        </div>
       </div>
-    </div>
+    );
+  }
+  return (
+    <>
+      Signed in as {session.user.email} <br />
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
   );
 }
