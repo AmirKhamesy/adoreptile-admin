4b8c7be36b4cf0c34a4be6406f843a81b3fff08b
Amir <iknubs@gmail.com>
2023-04-29 15:22:00 -0700

    outline of file/image uploader done

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,6 +7,7 @@ export default function ProductForm({
   title: currentTitle,
   description: currentDescription,
   price: currentPrice,
+  images,
 }) {
   const [title, setTitle] = useState(currentTitle || "");
   const [description, setDescription] = useState(currentDescription || "");
@@ -17,8 +18,10 @@ export default function ProductForm({
     e.preventDefault();
     const data = { title, description, price };
     if (_id) {
+      // Update
       await axios.put("/api/products", { ...data, _id });
     } else {
+      // Create
       await axios.post("/api/products", data);
     }
     setReturnHome(true);
@@ -26,6 +29,18 @@ export default function ProductForm({
 
   if (returnHome) {
     router.push("/products");
+  }
+
+  async function uploadImages(e) {
+    const files = e.target?.files;
+    if (files?.length > 0) {
+      const data = new FormData();
+      files.forEach((file) => {
+        data.append("file", file);
+      });
+      const response = await axios.post("/api/upload");
+      console.log(response.data);
+    }
   }
 
   return (
@@ -37,6 +52,28 @@ export default function ProductForm({
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       ></input>
+      <label>Photo</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 text-center flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input onChange={uploadImages} type="file" className="hidden"></input>
+        </label>
+        {!images?.length && <div> No photos</div>}
+      </div>
       <label>Description</label>
       <textarea
         placeholder="Description"
